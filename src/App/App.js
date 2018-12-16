5940590186b2e7a38c220246ecc715f11a1a4517
unknown <jansihandy@gmail.com>
2018-12-16 22:23:45 +0100

    update

---
+++ src/App/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Timer from '../Timer/Timer'
+
+class App extends Component {
+	render() {
+    	return (
+      		<div className="App">
+				
+				<Timer />
+
+    		</div>
+    	);
+	}
+}
+
+export default App;
